7fbda1d7e1b9daaecaf5f1a225222e95ca25a5ba
Vivek <vkg1257@gmail.com>
2025-04-04 11:20:28 +0530

    redirect bug resolve

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist', // Ensure build output is placed in `dist/`
+  },
+  server: {
+    historyApiFallback: true, // Enables proper routing
+  },
 })
