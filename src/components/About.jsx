bac56f4ad7a3bc1c4d384eebe908d6a8e4e385a0
vivek <vkg1257@gmail.com>
2025-05-20 23:24:27 +0530

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,7 @@ const AboutUs = () => {
             <h1>About Us</h1>
             <p>We provide innovative tech solutions to help businesses grow and succeed in the digital era.</p>
         </div>
+        
     );
 };
 
